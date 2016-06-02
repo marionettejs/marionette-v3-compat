46fc571984590af97cbfe3209f0f175c177eb63d
wesmangum <wesmangum24@gmail.com>
2016-06-02 11:31:11 -0500

    create RegionManager.js and Controller.js

--- src/marionette-v3-compat.js
+++ src/marionette-v3-compat.js
@@ -1,18 +1,22 @@
 import _ from 'underscore';
 import Marionette from 'backbone.marionette';
 
+import Controller from './Controller';
 import ItemView from './ItemView';
 import LayoutView from './LayoutView';
 import Module from './Module';
+import RegionManager from './RegionManager';
 import templateHelpers from './templateHelpers';
 
 Marionette.VERSION = 'marionette-v3-compat';
 
 export default function(opts = {}) {
   const patches = _.extend({
+    Controller,
     ItemView,
     LayoutView,
     Module,
+    RegionManager,
     templateHelpers
   }, opts);
 
