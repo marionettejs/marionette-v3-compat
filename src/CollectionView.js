5768ef353edc7f840b010be20943fb100235b210
Wes Mangum <wesmangum24@gmail.com>
2016-06-06 19:30:25 -0500

    Collection view implementation (#4)
    * create restoreFunction util and CollectionView
    
    * Create CollectionView and View getChildView
    
    * update getChildView to use restoreFunction util

---
+++ src/CollectionView.js
@@ -0,0 +1,28 @@
+import Marionette from 'backbone.marionette';
+import restoreFunction from './utils/restoreFunction';
+export default function() {
+
+  restoreFunction('_endBuffering', 'endBuffering', 'endBuffering is now private.', 'CollectionView');
+  restoreFunction('_startBuffering', 'startBuffering', 'startBuffering is now private.', 'CollectionView');
+  restoreFunction('_showCollection', 'showCollection', 'showCollection is now private.', 'CollectionView');
+  restoreFunction('_showEmptyView', 'showEmptyView', 'showEmptyView is now private.', 'CollectionView');
+  restoreFunction('_destroyEmptyView', 'destroyEmptyView', 'destroyEmptyView is now private.', 'CollectionView');
+  restoreFunction('_checkEmpty', 'checkEmpty', 'checkEmpty is now private.', 'CollectionView');
+  restoreFunction('_destroyChildren', 'destroyChildren', 'destroyChildren is now private.', 'CollectionView');
+  restoreFunction('_proxyChildEvents', 'proxyChildEvents', 'proxyChildEvents is now private.', 'CollectionView');
+  restoreFunction('_addChild', 'addChild', 'addChild is now private.', 'CollectionView');
+
+  const originalRemoveChildView = Marionette.CollectionView.prototype.removeChildView;
+
+  Marionette.CollectionView = Marionette.CollectionView.extend({
+    initRenderBuffer() {
+      Marionette.deprecate('initRenderBuffer is now private.');
+      this._bufferedChildren = [];
+    },
+    removeChildView(view) {
+      Marionette.deprecate('removeChildView no longer returns the view.');
+      originalRemoveChildView.apply(this, arguments);
+      return view;
+    }
+  });
+}
---
+++ src/getChildView.js
@@ -0,0 +1,22 @@
+import Marionette from 'backbone.marionette';
+import restoreFunction from './utils/restoreFunction';
+
+export default function() {
+
+  const originalPublicGetChildView = Marionette.View.prototype.getChildView;
+
+  restoreFunction('_getChildView', 'getChildView', 'getChildView is deprecated. Use childView instead.', 'CollectionView');
+  restoreFunction('_getChildView', 'getChildView', 'getChildView is deprecated. Use childView instead.', 'View');
+
+  const restoredPublicGetChildView = Marionette.View.prototype.getChildView;
+
+  Marionette.View = Marionette.View.extend({
+    getChildView() {
+      if (arguments.length) {
+        return originalPublicGetChildView.apply(this, arguments);
+      }
+
+      return restoredPublicGetChildView.apply(this, arguments);
+    }
+  });
+}
