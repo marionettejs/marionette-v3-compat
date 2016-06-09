9d0244692c83f80dcc211cf55bd98df367249793
wesmangum <wesmangum24@gmail.com>
2016-06-09 15:31:25 -0500

    pass this to render:children triggerMethod

--- src/CollectionView.js
+++ src/CollectionView.js
@@ -16,7 +16,7 @@ export default function() {
 
   Marionette.CollectionView = Marionette.CollectionView.extend({
     constructor() {
-      this.on('render:collection', function() { this.triggerMethod('render:children'); });
+      this.on('render:collection', function() { this.triggerMethod('render:children', this); });
       originalConstructor.apply(this, arguments);
     },
     initRenderBuffer() {
