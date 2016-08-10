39222e69df02ceceef26f0986993cba1dba1dc2f
Paul Falgout <paul@otterball.com>
2016-08-10 15:51:35 +0900

    Fix getChildView / getEmptyView
    was some confusion here with View’s function by the same name

--- src/getChildView.js
+++ src/getChildView.js
@@ -1,22 +1,7 @@
-import Marionette from 'backbone.marionette';
 import restoreFunction from './utils/restoreFunction';
 
 export default function() {
 
-  const originalPublicGetChildView = Marionette.View.prototype.getChildView;
-
   restoreFunction('_getChildView', 'getChildView', 'getChildView is deprecated. Use childView instead.', 'CollectionView');
-  restoreFunction('_getEmptyView', 'getEmptyView', 'getEmptyView is deprecated. Use emptyView instead.', 'View');
-
-  const restoredPublicGetChildView = Marionette.View.prototype.getChildView;
-
-  Marionette.View = Marionette.View.extend({
-    getChildView() {
-      if (arguments.length) {
-        return originalPublicGetChildView.apply(this, arguments);
-      }
-
-      return restoredPublicGetChildView.apply(this, arguments);
-    }
-  });
+  restoreFunction('_getEmptyView', 'getEmptyView', 'getEmptyView is deprecated. Use emptyView instead.', 'CollectionView');
 }
